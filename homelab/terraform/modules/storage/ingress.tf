71d86609313f45666b235e23687dae3c99d0eadd
cmj <cmj@cmj.tw>
2023-11-05 23:11:19 +0800

    fix(IaC|modules): correct the template

--- homelab/terraform/modules/storage/ingress.tf
+++ homelab/terraform/modules/storage/ingress.tf
@@ -1,7 +1,8 @@
 resource "kubernetes_manifest" "redis-vs" {
-  manifest = yamldecode(templatefile("${path.module}/templates/redis-ingress.yml", {
+  manifest = yamldecode(templatefile("${path.module}/templates/virtual_service.yml", {
     name      = "redis-vs"
     namespace = local.namespace
+    hostname  = module.redis.hostname
 
     gateway      = kubernetes_manifest.storage-gw.manifest.metadata.name
     service_name = module.redis.service_name
