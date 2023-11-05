a1fdc0800fb8ddf15dfd4a53637d98f94e3a17ad
cmj <cmj@cmj.tw>
2023-11-05 21:08:16 +0800

    refactor(IaC): move monitoring into a standalone tf module

--- homelab/terraform/modules/based/main.tf
+++ homelab/terraform/modules/based/main.tf
@@ -1,13 +1,15 @@
-module "networking" {
-  source   = "./networking"
+module "pihole" {
+  source   = "./pihole"
   hostname = var.hostname
 }
 
-module "monitoring" {
-  source   = "./monitoring"
-  hostname = var.hostname
+module "external-dns" {
+  source                = "./external-dns"
+  hostname              = var.hostname
+  pihole_service_name   = module.pihole.service_name
+  pihole_admin_password = module.pihole.admin_password
 
   depends_on = [
-    module.networking,
+    module.pihole.pi_hole,
   ]
 }
--- homelab/terraform/modules/based/networking/output.tf
+++
@@ -1,5 +0,0 @@
-output "pihole_admin_password" {
-  description = "The admin password for the Pi-hole web interface"
-  value       = module.pihole.admin_password
-  sensitive   = true
-}
