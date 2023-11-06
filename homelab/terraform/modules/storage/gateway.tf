f9c189fc878e9214ba9c9f3dd4dd81868c1956a1
cmj <cmj@cmj.tw>
2023-11-06 11:35:35 +0800

    refactor(IaC|modules): export storage services

--- homelab/terraform/modules/storage/gateway.tf
+++
@@ -1,6 +0,0 @@
-resource "kubernetes_manifest" "storage-gw" {
-  manifest = yamldecode(templatefile("${path.module}/templates/gateway.yml", {
-    name      = "storage-gw"
-    namespace = local.namespace
-  }))
-}
--- homelab/terraform/modules/storage/postgresql/main.tf
+++ homelab/terraform/modules/storage/postgresql/main.tf
@@ -3,6 +3,7 @@ locals {
   version = "13.2.1"
 
   hostname = "${local.name}.${var.hostname}"
+  port     = 5432
 }
 
 resource "helm_release" "postgresql" {
@@ -14,6 +15,27 @@ resource "helm_release" "postgresql" {
     storage_class_name = var.storage_class_name
     password           = random_password.password.result
   })]
+}
+
+resource "kubernetes_manifest" "gateway" {
+  manifest = yamldecode(templatefile("${path.module}/../templates/gateway.yml", {
+    name      = "${local.name}-gw"
+    namespace = var.namespace
+
+    service_port = local.port
+  }))
+}
+
+resource "kubernetes_manifest" "virtual-service" {
+  manifest = yamldecode(templatefile("${path.module}/../templates/virtual_service.yml", {
+    name      = "${local.name}-vs"
+    namespace = var.namespace
+
+
+    gateway      = kubernetes_manifest.gateway.manifest.metadata.name
+    service_name = "${local.name}"
+    service_port = local.port
+  }))
 }
 
 resource "random_password" "password" {
