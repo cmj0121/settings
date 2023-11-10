8ddbafdef07ce8635d1d4193e3664933c1f72317
cmj <cmj@cmj.tw>
2023-11-10 15:52:04 +0800

    feat(IaC|plugins): refine the service entry

--- homelab/terraform/plugins/drone/oauth_app.tf
+++ homelab/terraform/plugins/drone/oauth_app.tf
@@ -16,21 +16,34 @@ resource "gitea_oauth2_app" "drone-oauth-app" {
 }
 
 resource "kubernetes_manifest" "gitea-service-entry" {
-  manifest = yamldecode(templatefile("${path.module}/values/gitea-service-entry.yml", {
+  manifest = yamldecode(templatefile("${path.module}/values/service-entry.yml", {
+    name      = "gitea-hostname-alias"
     namespace = var.namespace
 
-    gitea_hostname     = var.gitea_hostname
-    gitea_service_name = var.gitea_service_name
-    gitea_service_port = var.gitea_service_port
+    hostname     = var.gitea_hostname
+    service_name = var.gitea_service_name
+    service_port = var.gitea_service_port
   }))
 }
 
 resource "kubernetes_manifest" "registry-service-entry" {
-  manifest = yamldecode(templatefile("${path.module}/values/registry-service-entry.yml", {
+  manifest = yamldecode(templatefile("${path.module}/values/service-entry.yml", {
+    name      = "registry-hostname-alias"
     namespace = var.namespace
 
-    registry_hostname     = var.registry_hostname
-    registry_service_name = var.registry_service_name
-    registry_service_port = var.registry_service_port
+    hostname     = var.registry_hostname
+    service_name = var.registry_service_name
+    service_port = var.registry_service_port
+  }))
+}
+
+resource "kubernetes_manifest" "drone-service-entry" {
+  manifest = yamldecode(templatefile("${path.module}/values/service-entry.yml", {
+    name      = "drone-hostname-alias"
+    namespace = var.namespace
+
+    hostname     = local.hostname
+    service_name = "drone"
+    service_port = 8080
   }))
 }
