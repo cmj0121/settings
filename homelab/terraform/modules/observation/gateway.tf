e47b121f72409a371f4dfa55d86a17cc5b32c6f6
cmj <cmj@cmj.tw>
2024-10-25 10:13:04 +0800

    feat(IaC|module): remove the observation

--- homelab/terraform/modules/observation/gateway.tf
+++
@@ -1,7 +0,0 @@
-resource "kubernetes_manifest" "monitoring-gw" {
-  manifest = yamldecode(templatefile("${path.module}/templates/gateway.yml", {
-    name      = "monitoring-gw"
-    namespace = local.namespace
-    hostname  = var.hostname
-  }))
-}
